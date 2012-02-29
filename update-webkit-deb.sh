5a0e91cedb2e4334d8da1b55c9830ad204a00f82
Lauro Neto <lauro.neto@openbossa.org>
2012-02-29 14:52:50 -0300

    Parametrize

--- update-webkit-deb.sh
+++ update-webkit-deb.sh
@@ -1,27 +1,45 @@
 #!/bin/sh
 
-# find the right qmake
+# Updates the debian folder to the latest release. It assumes a webkit git clone
+# with a working 'debianize' branch with the debian folder. The version number
+# will be the latest '"$REMOTE"/master' SVN release.
+
+set -e
+
+BRANCH="debianize"
+DISTRIBUTION="unstable"
+REMOTE="gitorious"
+
+# Adjust the values below according to your scratchbox and webkit locations
+
 SBOXHOME=/scratchbox/users/$USERNAME/home/$USERNAME
 WEBKITFOLDER=qt5/webkit-deb
+
 cd "$SBOXHOME/$WEBKITFOLDER"
 
-git fetch
+git fetch "$REMOTE"
 
-commit=$(git log -n 1 --format=format:%H origin/master)
-svnrevision=$(git log -n 1 origin/master | grep git-svn-id | sed -E "s/.*@([0-9]*).*/\\1/")
-git checkout debianize
+commit=$(git log -n 1 --format=format:%H "$REMOTE"/master)
+svnrevision=$(git log -n 1 "$REMOTE"/master | grep git-svn-id | sed -E "s/.*@([0-9]*).*/\\1/")
+
+git checkout "$BRANCH"
 
 if git log --format=format:%H | grep -qs "$commit"; then
-	echo
+    # Already merged, just rebuild.
+    echo
 else
-	git merge origin/master
+    git merge "$REMOTE"/master
 
-	dch -D unstable --force-distribution -v "$svnrevision" "New snapshot."
+    dch -D "$DISTRIBUTION" --force-distribution -v "$svnrevision" "New snapshot."
 
-	git add debian/changelog
+    git add debian/changelog
 
-	git commit -m "New release."
+    git commit -m "New release."
 fi
 
 # finally build.
-echo "cd $WEBKITFOLDER; export PATH=/opt/qt5/bin:\$PATH; dpkg-buildpackage -rfakeroot -uc -us -B" | scratchbox -s
+scratchbox -s <<EOF
+cd $WEBKITFOLDER
+export PATH=/opt/qt5/bin:\$PATH
+dpkg-buildpackage -rfakeroot -uc -us -B 2>&1 | tee /tmp/build-webkit-$svnrevision.txt
+EOF
