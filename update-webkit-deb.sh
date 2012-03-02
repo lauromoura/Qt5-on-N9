9632c9e97f4432c5754c5a354c81261327478904
Lauro Neto <lauro.neto@openbossa.org>
2012-03-02 18:07:20 -0300

    Option to specify a webkit revision

--- update-webkit-deb.sh
+++ update-webkit-deb.sh
@@ -4,6 +4,8 @@
 # with a working 'debianize' branch with the debian folder. The version number
 # will be the latest '"$REMOTE"/master' SVN release.
 
+# usage: $0 [revision]
+
 set -e
 DIR="$(cd "$( dirname "$0")" && pwd )"
 
@@ -13,16 +15,25 @@ cd "$SBOXHOME/$WEBKITFOLDER"
 
 git fetch "$REMOTE"
 
-commit=$(git log -n 1 --format=format:%H "$REMOTE"/master)
-svnrevision=$(git log -n 1 "$REMOTE"/master | grep git-svn-id | sed -E "s/.*@([0-9]*).*/\\1/")
+if [ -z "$1" ];
+then
+    echo "Using latest snapshot"
+    commit=$(git log -n 1 --format=format:%H "$REMOTE"/master)
+    svnrevision=$(git log -n 1 "$REMOTE"/master | grep git-svn-id | sed -E "s/.*@([0-9]*).*/\\1/")
+else
+    echo "Using specific revision $1"
+    svnrevision="$1"
+    commit=$(git log -n 1 --format=format:%H --grep "trunk@$svnrevision" "$REMOTE"/master)
+fi
+
+echo "Revision $svnrevision and commit $commit"
 
 git checkout "$BRANCH"
 
 if git log --format=format:%H | grep -qs "$commit"; then
-    # Already merged, just rebuild.
-    echo
+    echo "Already merged, just rebuild."
 else
-    git merge "$REMOTE"/master
+    git merge "$commit"
 
     dch -D "$DISTRIBUTION" --force-distribution -v "$svnrevision" "New snapshot."
 
