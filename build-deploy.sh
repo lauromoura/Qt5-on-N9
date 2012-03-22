e19b7f9e8145a61e58e75e69b81e215e50518d59
Lauro Neto <lauro.neto@openbossa.org>
2012-03-22 17:59:52 -0300

    Build stuff withouth bothering with dpkg.
    Copies the current project folder to sbox/<project>-tmp,
    builds, and scp the <project> executable to the device.
    
    Kudos to Rafael Brandao for the ideas.

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,35 +1,27 @@
 #!/bin/sh
 
-PROJECTFOLDER_HOST=$(pwd -P)
-PROJECTFOLDER=${PROJECTFOLDER_HOST#/scratchbox/users/$USER}
 
-LOGFILE=/tmp/$(basename $PROJECTFOLDER).txt
+SBOX_HOME=/scratchbox/users/$USER/home/$USER
+PROJECTFOLDER_HOST=$(pwd -P)
+PROJECTFOLDER=$(basename $PROJECTFOLDER_HOST)-tmp
+
+LOGFILE=/tmp/$PROJECTFOLDER.txt
 
 # N9 configuration.
 N9=developer@192.168.2.15
 N9HOME=/home/developer
 
-# Build deb
+echo "rsync $PROJECTFOLDER_HOST to $SBOX_HOME/$PROJECTFOLDER"
+rsync -azrptL --progress --exclude-from=".gitignore" --exclude ".git" $PROJECTFOLDER_HOST/ $SBOX_HOME/$PROJECTFOLDER
+
+# Build project
 scratchbox -s <<EOF
 cd $PROJECTFOLDER
 export PATH=/opt/qt5/bin:\$PATH
-dpkg-buildpackage -rfakeroot -uc -us -B 2>&1 | tee $LOGFILE
+make distclean
+qmake
+make -j 3
 EOF
 
-# Get the package name
-DEBFILE=$(grep -o [-a-z0-9]*_[.0-9a-z-]*_armel\.deb $LOGFILE)
-
-echo "Sending file $DEBFILE to device."
-
-if [ -z "$DEBFILE" ]; then
-	echo "Failed to find the debian package."
-	exit 1;
-fi
-
-# Send to device and install
-scp $PROJECTFOLDER_HOST/../$DEBFILE $N9:$N9HOME
-
-# Do not spoil the root password :)
-stty -echo
-ssh $N9 "devel-su -c \"dpkg -i $N9HOME/$DEBFILE\""
-stty echo
+#FIXME Assuming executable name is the same as the folder
+scp $SBOX_HOME/$PROJECTFOLDER/${PROJECTFOLDER%-tmp} $N9:$N9HOME
