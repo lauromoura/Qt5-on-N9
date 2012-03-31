c4d66f8e8042ba33ade02e81005cd38641c3a265
Lauro Neto <lauro.neto@openbossa.org>
2012-03-31 16:11:26 -0300

    Avoid using debian default CFLAGS.
    It'd force -O2 on all compiler calls, causing a segfault
    when compiling uic, which is built without -O2.

--- build-qt-debs.sh
+++ build-qt-debs.sh
@@ -10,7 +10,8 @@ for i in base jsbackend xmlpatterns script declarative quick1 3d jsondb location
 # FIXME hardcoded repo paths
 scratchbox -s <<EOF
 export PATH=/opt/qt5/bin:\$PATH
-export DEB_BUILD_OPTIONS="noopt"
+export CFLAGS=" "
+export CPPFLAGS=" "
 cd qt/qt5-debs/qt$i
 dpkg-buildpackage -rfakeroot -uc -us 2>&1 | tee /tmp/build-$i.log
 fakeroot dpkg -i ../*$i*.deb
