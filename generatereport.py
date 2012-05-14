a43d2d1d7dba574e4ba91f749043812292c1b406
Lauro Neto <lauro.neto@openbossa.org>
2012-05-14 09:21:08 -0300

    Initial update in generatereport to handle filenames

--- generatereport.py
+++ generatereport.py
@@ -3,10 +3,11 @@ from jinja2 import Environment, FileSystemLoader
 
 def main():
     env = Environment(loader=FileSystemLoader('templates'))
-    template = env.get_template('template.html')
-    data = json.load(open('results.json'))
-    with open('results.html', 'w') as output:
-        output.write(template.render(bot="Qt Linux Release", data=data))
+    template = env.get_template('gchart_template.html')
+    bot = "Qt Linux Release"
+    botname = bot.replace(" ", "").lower()
+    with open('%s_results.html' % botname, 'w') as output:
+        output.write(template.render(bot=bot, botname=botname))
 
 if __name__ == '__main__':
     main()
