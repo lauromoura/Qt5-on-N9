b5620b5335630859bd9eb1089aa3029a946d6067
Lauro Neto <lauro.neto@openbossa.org>
2012-05-14 10:42:43 -0300

    Also unset CXXFLAGS

--- build-qt-debs.sh
+++ build-qt-debs.sh
@@ -12,6 +12,7 @@ scratchbox -s <<EOF
 export PATH=/opt/qt5/bin:\$PATH
 export CFLAGS=" "
 export CPPFLAGS=" "
+export CXXFLAGS=" "
 cd qt/qt5-debs/qt$i
 dpkg-buildpackage -rfakeroot -uc -us 2>&1 | tee /tmp/build-$i.log
 fakeroot dpkg -i ../*$i*.deb
